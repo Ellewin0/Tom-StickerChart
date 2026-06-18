fc8f3b2023265e23aeb667c6e431d7bf77596421
Ellewin0 <e.lewin@ecu.edu.au>
2026-06-18 20:08:05 +0800

    Add Firebase configuration to app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyATw1DuMrVzrKeb8Ebj3iVxQf-eafgG2bk",
+  authDomain: "tom-sticker-chart.firebaseapp.com",
+  databaseURL: "https://tom-sticker-chart-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tom-sticker-chart",
+  storageBucket: "tom-sticker-chart.firebasestorage.app",
+  messagingSenderId: "639564337276",
+  appId: "1:639564337276:web:d8bee0a7206ecefdd7b9f9"
+};
