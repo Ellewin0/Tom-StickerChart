e307ee42b9f7ea687325af83718b11c137b01e8b
Ellewin0 <e.lewin@ecu.edu.au>
2026-06-19 00:15:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -310,19 +310,23 @@ document.body.removeChild(link);
 }
 
 function drawRoundedRect(ctx, x, y, width, height, radius, color) {
+const safeRadius = Math.min(radius, width / 2, height / 2);
+
 ctx.beginPath();
-ctx.moveTo(x + radius, y);
-ctx.lineTo(x + width - radius, y);
-ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-ctx.lineTo(x + width, y + height - radius);
-ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-ctx.lineTo(x + radius, y + height);
-ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-ctx.lineTo(x, y + radius);
-ctx.quadraticCurveTo(x, y, x + radius, y);
+ctx.moveTo(x + safeRadius, y);
+ctx.lineTo(x + width - safeRadius, y);
+ctx.quadraticCurveTo(x + width, y, x + width, y + safeRadius);
+ctx.lineTo(x + width, y + height - safeRadius);
+ctx.quadraticCurveTo(x + width, y + height, x + width - safeRadius, y + height);
+ctx.lineTo(x + safeRadius, y + height);
+ctx.quadraticCurveTo(x, y + height, x, y + height - safeRadius);
+ctx.lineTo(x, y + safeRadius);
+ctx.quadraticCurveTo(x, y, x + safeRadius, y);
 ctx.closePath();
+
 ctx.fillStyle = color;
 ctx.fill();
+
 }
 
 function drawMiniCar(ctx, x, y) {
